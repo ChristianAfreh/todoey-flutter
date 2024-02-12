33271faad9678009a298aae642080a6f7dedc065
Christian Afreh <christian.afreh@primustechgh.net>
2024-02-12 12:44:27 +0000

    completed AddTask screen

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -8,12 +8,44 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: const Color(0xff757575),
       child: Container(
+        padding: const EdgeInsets.all(20.0),
         decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            )),
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: null,
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+              ),
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
